1ee94c07345ebddcbf218d98e4a6702b98ce344e
Omar Zambrano <omar.zmbrn1@gmail.com>
2025-11-26 23:10:53 -0600

    build: added typescript support

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000
-    }
+  plugins: [react({
+    include: ['**/*.{js,jsx,ts,tsx}'] // Add TypeScript support
+  })],
+  server: {
+    port: 3000
+  }
 })
